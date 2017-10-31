0192950ef2d5975a55227e972ebe822844681db1
xiongwengang <xiongwengang@bilibili.com>
2017-10-31 14:27:39 +0800

    refs 整理

--- study/ele/src/element/index.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue';
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-default/index.css';
-
-Vue.use(ElementUI);
--- vue_cli/template/mutations.js
+++
@@ -1,9 +0,0 @@
-import {MutationNameSpace} from '../../../lib/namespage'
-
-// const STATE_TEST = (state, test) => {
-//   state.test = test
-// }
-
-export default MutationNameSpace([XXX], {
-  // STATE_TEST
-})
